f809ca7f24211042f4a37da7d4ef2da06542ec1f
irve354e <irve354e@login1.alpha.hpc.tu-dresden.de>
2025-03-28 01:23:51 +0100

    simple job for train

--- src/train_job.sh
+++ src/train_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=logs/qlora_training_%j.err
 #SBATCH --partition=alpha            
 #SBATCH --gres=gpu:1          
-#SBATCH --time=00:10:00            
+#SBATCH --time=05:00:00            
 #SBATCH --mem=32G                 
 #SBATCH --cpus-per-task=4     
 
@@ -13,7 +13,6 @@ module load CUDA/11.7.0
 source /software/rome/r24.04/Miniconda3/24.7.1-0/etc/profile.d/conda.sh
 conda activate universal-ner
 
-# ?
 export CUDA_VISIBLE_DEVICES=0
 
 python quantize_train.py
